cd0dc69435ad60fbeec71aa11275562c0c2c67d6
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-17 22:45:03 +0200

    Notre Composant Title

--- tes_expo/components/Title.js
+++ tes_expo/components/Title.js
@@ -1,6 +1,33 @@
 import React from "react";
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
-const Title = (props) => {
-    return 
+const Title = ({title}) => {
+
+    const { container, bookTitle } = styles;
+
+    return (
+
+        <View style={container}>
+            <Text style={bookTitle}>{title}</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    bookTitle: {
+        color: "white",
+        fontFamily: "GT-Sectra-Fine-Regular",
+        fontSize: 30,
+        fontWeight: '400',
+        lineHeight: 30,
+        marginTop: 40
+
+    }
+});
+
+export default Title;
