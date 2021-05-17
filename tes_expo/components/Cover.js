fffffd453eb2e3050a87928096ba1bb69b0f1351
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-17 10:42:47 +0200

    Notre Composant Cover

---
+++ tes_expo/components/Cover.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+
+// {image} = props.image
+
+const Cover = ({image}) => {
+
+    const { container, imageStyle } = styles;
+
+    return (
+
+        <View style={container}>
+            <Image style={imageStyle} source={image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: 162,
+        height: 243
+    },
+    imageStyle: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 10
+    }
+});
+
+export default Cover;
