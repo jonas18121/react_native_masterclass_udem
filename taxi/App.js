8afed5864c15bd32cf96ec4174a5752cb7f30a41
Jonas18121 <jonasxtrm971@gmail.com>
2021-05-19 12:31:18 +0200

    projet taxi

---
+++ taxi/App.js
@@ -0,0 +1,37 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, { useEffect }from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// screens
+import LoginScreen from './src/screens/LoginScreen';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function App() {
+
+    useEffect(() => {
+        StatusBar.setBackgroundColor("yellow");
+    }, []);
+
+    return (
+      
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name='Login' component={LoginScreen} />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
